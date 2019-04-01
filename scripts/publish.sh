79058e9b38953ac495166a77b28a28af76238774
Sean Sundberg <seansund@us.ibm.com>
2019-04-01 11:24:07 -0500

    Updates scripts to check for existing image before pushing and for npm-version to call push

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,7 +3,7 @@
 SCRIPT_DIR=$(dirname $0)
 
 IMAGE_NAME="$1"
-IMAGE_VERSION="$2"
+IMAGE_VER="$2"
 REGISTRY_NAME="$3"
 
 if [[ -z "${IMAGE_NAME}" ]]; then
@@ -11,7 +11,7 @@ if [[ -z "${IMAGE_NAME}" ]]; then
   exit 1
 fi
 
-if [[ -z "${IMAGE_VERSION}" ]]; then
+if [[ -z "${IMAGE_VER}" ]]; then
   echo "IMAGE_VERSION is required as the second argument"
   exit 1
 fi
@@ -21,4 +21,16 @@ if [[ -z "${REGISTRY_NAME}" ]]; then
   exit 1
 fi
 
-${SCRIPT_DIR}/build-kube.sh ${REGISTRY_NAME} ${IMAGE_NAME} ${IMAGE_VERSION}
+if [[ -n $(docker image ls ${REGISTRY_NAME}/${IMAGE_NAME}:${IMAGE_VER}) ]]; then
+  echo -e "The image \033[1;32m${REGISTRY_NAME}/${IMAGE_NAME}:${IMAGE_VER}\033[0m already exists"
+  echo -en "Do you want to (o)verwrite the existing image or e(x)it? \033[1;33m[x] | o?\033[0m "
+  read option
+  if [[ "${option}" == "o" ]] || [[ "${option}" == "O" ]]; then
+    echo -e "You chose to \033[1;31moverwrite\033[0m, proceeding with push"
+  else
+    echo -e "Exiting... Run \033[1;32mnpm version [option]\033[0m to update the version and initiate the push"
+    exit 1
+  fi
+fi
+
+${SCRIPT_DIR}/build-kube.sh ${REGISTRY_NAME} ${IMAGE_NAME} ${IMAGE_VER}
